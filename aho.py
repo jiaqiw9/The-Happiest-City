7658b79413d9c7b50ff8fc63d29f432b8a74c4fe
bing xu <bingxu.au@gmail.com>
2021-04-08 14:02:15 +1000

    added an implementation of aho-corasick to perform matching

---
+++ aho.py
@@ -0,0 +1,79 @@
+
+class AhoNode:
+    def __init__(self):
+        self.goto = {}
+        self.out = []
+        self.fail = None
+
+def aho_create_forest(patterns):
+    root = AhoNode()
+
+    for path in patterns:
+        node = root
+        for symbol in path:
+            node = node.goto.setdefault(symbol, AhoNode())
+        node.out.append(path)
+    return root
+
+
+def aho_create_statemachine(patterns):
+    root = aho_create_forest(patterns)
+    queue = []
+    for node in root.goto.values():
+        queue.append(node)
+        node.fail = root
+
+    while len(queue) > 0:
+        rnode = queue.pop(0)
+
+        for key, unode in rnode.goto.items():
+            queue.append(unode)
+            fnode = rnode.fail
+            while fnode != None and key not in fnode.goto.keys():
+                fnode = fnode.fail
+            unode.fail = fnode.goto[key] if fnode else root
+            unode.out += unode.fail.out
+
+    return root
+
+
+def aho_find_all(s, root):
+    node = root
+    patterns = []
+    for i in range(len(s)):
+        while node != None and s[i] not in node.goto.keys():
+            node = node.fail
+        if node == None:
+            node = root
+            continue
+        node = node.goto[s[i]]
+        for pattern in node.out:
+            # Only match complete matches
+            if i + 1 == len(s) or s[i+1] == " " or s[i+1] == "," or s[i+1] == "!" or s[i+1] == "?" or s[i+1] == "." or s[i+1]=="'" or s[i+1] == "'":
+                patterns.append(pattern)
+                print("Matched with " + pattern)
+    return patterns
+
+
+def read_AFINN(filename: str) -> dict:
+    '''
+    Converts AFINN.txt into a dictionary mapping words to sentiment scores
+    '''
+    # read AFINN dictionary
+    word_scores = {}
+    with open(filename) as AFINN:
+        for line in AFINN:
+            line_split = line.rsplit(maxsplit=1)
+            word, score = line_split[0], int(line_split[1])
+            word_scores[word] = score
+    return word_scores
+
+
+strings = ["I can't stand being cancelled", "I can't be cancelled"]
+AFINN = read_AFINN("AFINN.txt")
+root = aho_create_statemachine(AFINN.keys())
+
+for i in strings:
+    print("String to search: ", i)
+    found = aho_find_all(i, root)
+    print(found)
