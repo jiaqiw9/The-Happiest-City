68a1c086fd8f13371065f124826067647586eb7f
bing xu <bingxu.au@gmail.com>
2021-04-08 18:29:44 +1000

    added a logger and fixed error occuring when .json was split in bad sections

--- parser.py
+++ parser.py
@@ -1,9 +1,11 @@
+import logging
 from mpi4py import MPI
 import re
 import time
 import sys
 import math
 import aho
+from logger import Logger
 
 # Constants for assigning a coordinate to a region on the GRID
 X_MIN, X_MAX = 144.7, 145.45
@@ -21,9 +23,11 @@ TWEET_REGEX = b'doc":.*?"text".*?".*?"(?:,).*"lang".*?,'
 # Regex to extract the coordinate from a tweet.
 COORD_REGEX = b'"geo".*?(?:"coordinates":)\[(.*?),(.*?)\]'
 
-
 # MAX buffer size
 MAX_BUFF = int((2**31 - 1) / 2)
+
+# Initalize logger
+logger = Logger("master")
 
 
 def read_AFINN(filename: str) -> dict:
@@ -51,10 +55,11 @@ def setup_AFINN(rank, comm):
     word_scores = comm.bcast(word_scores, root=0)
     return word_scores
 
-# Bottleneck not here
-
 
 def setup_grid_scores():
+    '''
+    Builds a dictionary to store the sentiment score and number of tweets observed for each grid cell
+    '''
     grid_scores = {}
     keys = ["A1", "A2", "A3", "A4", 'B1', 'B2',
             'B3', 'B4', 'C1', 'C2', 'C3', 'C4',
@@ -69,15 +74,15 @@ def master(comm, file_name):
     Main function for the master processor
     '''
     rank, size = comm.Get_rank(), comm.Get_size()
-
-    final_result = parse_tweets(file_name, rank, comm)
+    final_result = parse_tweets(file_name, rank, comm, size)
 
     if size != 1:
-        print("Number of processes: {}".format(size))
+        logger.log("Number of processes: {}".format(size))
         slave_results = collect_results(comm)
         add_slave_output(slave_results, final_result)
+        complete_jobs(comm, size - 1)
 
-    complete_jobs(comm, size - 1)
+    # Log the results to stdout
     print_results(final_result)
 
 
@@ -88,11 +93,14 @@ def complete_jobs(comm, n_slaves):
     for i in range(n_slaves):
         slave_rank = i + 1
         comm.send('complete', dest=slave_rank, tag=slave_rank)
-        print("Notifying slave {} that the process is complete".format(slave_rank))
+        logger.log(
+            "Notifying slave {} that the process is complete".format(slave_rank))
 
 
-# Bottleneck not hjere
 def collect_results(comm):
+    '''
+    Calls each slave and gathers the results from all slaves into one dictionary
+    '''
     n_slaves = comm.Get_size() - 1
     # Initialize a fresh dictionary to store the results from all the slaves in
     slave_results = setup_grid_scores()
@@ -100,13 +108,13 @@ def collect_results(comm):
     for i in range(n_slaves):
         slave_rank = i + 1
         comm.send('return', dest=slave_rank, tag=slave_rank)
-        print("Calling slave {} to return data".format(slave_rank))
+        logger.log("Calling slave {} to return data".format(slave_rank))
 
     for i in range(n_slaves):
         slave_rank = i + 1
         slave_output = comm.recv(source=slave_rank, tag=0)
         add_slave_output(slave_output, slave_results)
-        print("Succesfully received output from slave {}".format(slave_rank))
+        logger.log("Succesfully received output from slave {}".format(slave_rank))
 
     return slave_results
 
@@ -118,73 +126,86 @@ def add_slave_output(slave_output, slave_results):
 
 
 def slave(comm, file_name):
+    '''
+    Main function responsible for dictating behaviour of workers
+    '''
     rank, size = comm.Get_rank(), comm.Get_size()
-
     # Do work
-    output = parse_tweets(file_name, rank, comm)
-
+    output = parse_tweets(file_name, rank, comm, size)
     # Await call to return results to master
     while True:
         incoming_msg = comm.recv(source=0, tag=rank)
-        if incoming_msg == 'return':
-            comm.send(output, dest=0, tag=0)
-        elif incoming_msg == 'complete':
-            exit(0)
+        if isinstance(incoming_msg, str):
+            if incoming_msg == 'return':
+                comm.send(output, dest=0, tag=0)
+            elif incoming_msg == 'complete':
+                exit(0)
 
 
-def open_file(comm, fname):
-    # print("trying to open ", fname)
-    file = MPI.File.Open(comm, fname, MPI.MODE_RDONLY)
-    file_size = MPI.File.Get_size(file)
-    return file, file_size
+def open_file(comm, fname, rank):
+    try:
+        file = MPI.File.Open(comm, fname, MPI.MODE_RDONLY)
+        file_size = MPI.File.Get_size(file)
+        return file, file_size
+    except BufferError:
+        logger.log_error("Unable to open file at worker {}".format(rank))
+        return None, 0
 
 
-def parse_tweets(file_name: str, rank, comm):
-    size = comm.Get_size()
-    # Set-up
-    word_scores = setup_AFINN(rank, comm)
-    root = aho.aho_create_statemachine(word_scores.keys())
-
-    grid_scores = setup_grid_scores()
-    # print("Doing work on processor {}".format(rank))
-
-    # Open the .json containing all tweets
-    file, file_size = open_file(comm, file_name)
-    # print("{} is {} bytes.".format(file_name, file_size))
-
-    # Chunk of file to read
-    if (size == 1 or size - 1 != rank):
-        chunk_size = int(math.ceil(file_size/size))
-        chunk_offset = chunk_size * rank  # This is where the chunk to be read starts
-    else:
-        chunk_size = (file_size % int(math.ceil(file_size/size)))
-        chunk_offset = file_size - chunk_size - 1
-    # print("Using chunks of size {} to read in the .json. Chunk offset is {}".format(
-        # chunk_size, chunk_offset))
+def which_chunk(file_size, size, rank):
+    '''
+    Determines the chunk of the file the worker is responsible for processing 
+    '''
+    chunk_size = int(math.ceil(file_size/size))
+    chunk_offset = chunk_size * rank  # This is where the chunk to be read starts
     # Find how many buffers are needed to read the chunk in
     n_buffers = int(math.ceil(chunk_size / MAX_BUFF))
-    # print("Number of buffers to use: {}. Buffer size: {}".format(
-    # n_buffers, buffer_size))
+    return chunk_size, chunk_offset, n_buffers
+
+def write_to_buffer(file, chunk_size, n_buffers, chunk_offset, buffer_index):
+    buffer_size = int(math.ceil(chunk_size / n_buffers))
+    buffer = bytearray(buffer_size)
+    buffer_offset = chunk_offset + (buffer_index * buffer_size)
+    file.Read_at_all(buffer_offset, buffer)
+    return buffer
+
+def parse_tweets(file_name: str, rank, comm, size):
+    # Set-up the data-structures needed to process the json
+    word_scores = setup_AFINN(rank, comm)
+    root = aho.aho_create_statemachine(word_scores.keys())
+    grid_scores = setup_grid_scores()
+    # Open the .json
+    file, file_size = open_file(comm, file_name, rank)
+    if not file:
+        return grid_scores
+    # Chunk of file to read
+    chunk_size, chunk_offset, n_buffers = which_chunk(file_size, size, rank)
+
+    logger.log("Commencing processing of chunk beginning at {} of {} of size {} at worker {}".format(
+        chunk_offset, file_size, chunk_size, rank))
+    
     for i in range(n_buffers):
         try:
-            # print("in n_buffers")
-            buffer_size = int(math.ceil(chunk_size / n_buffers))
-            buffer = bytearray(buffer_size)
-            buffer_offset = chunk_offset + (i * buffer_size)
-            # # Read the file into the buffer
-            file.Read_at_all(buffer_offset, buffer)
-            # # Find all the tweets contained in the buffer:
-            tweets = re.findall(TWEET_REGEX, buffer, re.I) ##bottleneck here
-            # # # Process each tweet
-            for tweet in tweets:
-                cell, score = parse_single_tweet(tweet, word_scores, root)
-                process_score(score, cell, grid_scores)
-                # print(" Cell: ", cell, " Sentiment score: ", score)
-        except:
-            print("Error opening file")
+            # Read the file into the buffer
+            buffer = write_to_buffer(file, chunk_size, n_buffers, chunk_offset, i)
+            # Find all the tweets contained in the buffer and process them
+            find_and_process_tweets(buffer, word_scores, grid_scores, root)
+            logger.log(
+                "Succesfully parsed file portion {}/{} at worker {}".format(i+1, n_buffers, rank))
+
+        except ValueError:
+            logger.log_error(
+                "Can't open file portion {}/{} at worker {}".format(i+1, n_buffers, rank))
     file.Close()
     return grid_scores
 
+
+def find_and_process_tweets(buffer, word_scores, grid_scores, root):
+    tweets = re.findall(TWEET_REGEX, buffer, re.I)
+    for tweet in tweets:
+        cell, score = parse_single_tweet(tweet, word_scores, root)
+        if cell:
+            process_score(score, cell, grid_scores)
 
 
 def parse_single_tweet(tweet, word_scores, root):
@@ -192,18 +213,25 @@ def parse_single_tweet(tweet, word_scores, root):
     Extracts the contents and co-ordinates of the tweet and parses the result
     '''
     text = re.search(TEXT_REGEX, tweet).group(1)
+    if not text:
+        logger.log_error("Issue with searching tweet for text")
+        return None, 0
     decoded = text.decode("utf-8")
     score = calculate_score(word_scores, decoded.lower(), root)
     coordinates = re.search(COORD_REGEX, tweet)
+    if not coordinates:
+        logger.log_error("Issue with searching tweet for co-ordinates")
+        return None, 0
     long = float(coordinates.group(1))
     lat = float(coordinates.group(2))
     cell = get_grid_cell(lat, long)
     return cell, score
 
-# Pattern matching using Aho-Corasick
+
 def calculate_score(word_scores: dict, tweet: str, root) -> int:
     '''
-    Calculates the sentiment score of a tweet
+    Calculates the sentiment score of a tweet. 
+    Pattern matching using modified Aho-Corasick to only match on exact matches
     '''
     score = 0
     patterns_found = aho.aho_find_all(tweet, root)
@@ -212,29 +240,30 @@ def calculate_score(word_scores: dict, tweet: str, root) -> int:
     return score
 
 
-
 def process_score(score, grid_cell, grid_scores):
     '''
     Increments the count of tweets encounter in a given cell and also 
     modifies the sentiment score of the cell accordingly
     '''
     if grid_cell == "A5" or grid_cell == "B5" or grid_cell == "D1" or grid_cell == "D2":
-        print("Error - we have an out of bounds cell being counted")
+        logger.log(
+            "Error - we have an out of bounds cell being counted - {}".format(grid_cell))
     grid_scores[grid_cell]['score'] += score
     grid_scores[grid_cell]['count'] += 1
 
 
 def get_grid_cell(x, y):
+    '''
+    Determines which grid cell a given point (represented by a latitude and longitude) belongs to.
+    '''
     if y > Y_MIN or y < Y_MAX or x < X_MIN or x > X_MAX or (y > -37.8 and x > 145.3) or (y < -37.95 and x < 145):
-        print("out of bounds")
-        return None
+        return None  # Out of bounds - not in Melbourne
     else:
         y_index = math.floor(abs(y - Y_MIN) / INTERVAL)
         x_index = math.floor(abs(x - X_MIN) / INTERVAL)
-        # Error
         if x_index > 4 or y_index > 3:
-            print(
-                "An error occurred when finding the grid associated with the point ({},{})".format(y, x))
+            logger.log_error(
+                "Error occurred when finding the grid cell associated with the point ({},{})".format(x, y))
             return None
         else:
             name = Y_ORDS[y_index] + X_ORDS[x_index]
@@ -242,20 +271,26 @@ def get_grid_cell(x, y):
 
 
 def print_results(result):
+    '''
+    Sends results to stdout in a clean format
+    '''
     for cell in result:
-        print("{}, Tweet count: {}, Sentiment score: {}".format(
+        logger.log("{}, Tweet count: {}, Sentiment score: {}".format(
             cell, result[cell]['count'], result[cell]['score']))
 
 
 def main(argv):
+    '''
+    Entrypoint into the process
+    '''
     fname = argv[0]
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     if rank == 0:
         start_time = time.time()
-        print("Starting execution at {}".format(time.asctime()))
+        logger.log("Starting execution at {}".format(time.asctime()))
         master(comm, fname)
-        print("Finished execution at {} - took {}".format(time.asctime(),
-                                                          time.time() - start_time))
+        logger.log("Finished execution at {} - took {}".format(time.asctime(),
+                                                               time.time() - start_time))
     else:
         slave(comm, fname)
